ee7823387c46c920f7b35a5f59f87635a7a93172
Mahurpawar, Sanjay <smahurpawar@verisign.com>
2015-05-08 11:27:30 +0530

    Added zone write commmand live test

---
+++ verisignmdns/src/test/java/denominator/verisignmdns/VerisignMdnsZoneWriteCommandsLiveTest.java
@@ -0,0 +1,9 @@
+package denominator.verisignmdns;
+
+import denominator.Live.UseTestGraph;
+import denominator.ZoneWriteCommandsLiveTest;
+
+@UseTestGraph(VerisignMdnsTestGraph.class)
+public class VerisignMdnsZoneWriteCommandsLiveTest extends ZoneWriteCommandsLiveTest {
+
+}
